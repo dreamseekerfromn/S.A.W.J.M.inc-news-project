fbd0045f7768185b88646094a9dc4f473ee76d8b
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-09-12 21:51:09 -0400

    Add weather.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  const weather = import.meta.env.VITE_weather_api;
-  console.log(bus);
   return <></>;
 }
 
