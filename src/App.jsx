180f6cad662ddf7a2148f0d476bd08ca860d1ace
Joseph-Carter <1joseph1carter@gmail.com>
2023-09-13 20:57:10 -0400

    added category filtering for news sources

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import Newspage from "./componets/Newspage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  
+  const NEWS = import.meta.env.VITE_NEWS_KEY;
+  // const BUS = import.meta.env.VITE_API_KEY;
+
   return (
-    <div className="App">
-      <h1>API Response</h1>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/newspage" element={<Newspage NEWS={NEWS} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
