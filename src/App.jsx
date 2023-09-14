4884128d5d08d5bdb96618b819d00828a1377a8a
Joseph-Carter <1joseph1carter@gmail.com>
2023-09-14 18:10:06 -0400

    creating about me page, working on contributor cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Newspage from "./componets/Newspage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./componets/NavBar"
+import AboutMe from "./componets/AboutMe";
 
 function App() {
   const NEWS = import.meta.env.VITE_NEWS_KEY;
@@ -13,6 +14,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Newspage NEWS={NEWS} />} />
+          <Route path="/about" element={<AboutMe />} />
         </Routes>
       </div>
     </Router>
---
+++ src/componets/AboutMe.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AboutMe = () => {
+
+  return (
+    <div>
+      <div className="row" style={{ paddingTop: "50px", listStyle: "none" }}>
+        <div className="col">
+          
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default AboutMe;
