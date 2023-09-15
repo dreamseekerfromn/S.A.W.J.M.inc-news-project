682186b4c7747941774271f446e29dba1bceaf03
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-09-15 19:19:10 -0400

    working for single MTA feed

--- src/components/MTAHeadline.jsx
+++ src/components/MTAHeadline.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { testAPI } from '../test/fetch';
 
-function MTA() {
+function MTAHeadline() {
     /** declare state hooks */
     /** initialize "data" state hook to same structure as the json from the MTA api */
     const [data, setData] = useState([
@@ -68,15 +68,90 @@ function MTA() {
         },
       },
     ]);
+
     const [loaded, setLoaded] = useState(false);
+    const [count, setCount] = useState(0);
+    const [singleFeed, setSingleFeed] = useState(
+      {
+        id: "",
+        alert: {
+          active_period: [
+            {
+              start: 0,
+              end: 0,
+            },
+          ],
+          informed_entity: [
+            {
+              agency_id: "",
+              route_id: "",
+              "transit_realtime.mercury_entity_selector": {
+                sort_order: "",
+              },
+            },
+          ],
+          header_text: {
+            translation: [
+              {
+                text: "",
+                language: "",
+              },
+              {
+                text: "",
+                language: "",
+              },
+            ],
+          },
+          description_text: {
+            translation: [
+              {
+                text: "",
+                language: "",
+              },
+              {
+                text: "",
+                language: "",
+              },
+            ],
+          },
+          "transit_realtime.mercury_alert": {
+            created_at: 0,
+            updated_at: 0,
+            alert_type: "",
+            display_before_active: 0,
+            screens_summary: {
+              translation: [
+                {
+                  text: "",
+                  language: "",
+                },
+                {
+                  text: "",
+                  language: "",
+                },
+              ],
+            },
+          },
+        },
+      }
+    )
+    const timerRef = useRef();
+    
+
+    /**
+     * getDataFromAPI()
+     * ----------------------------
+     * fetch from api & attatch to the state hook.
+     * 
+     */
+    async function getDataFromAPI(){
+      const resp = await testAPI()
+      const json = await resp.json()
+      setData([...json.entity]);
+    }
 
     /** fetch data from the API */
     useEffect(()=>{
-        async function getDataFromAPI(){
-            const resp = await testAPI()
-            const json = await resp.json()
-            setData([...json.entity]);
-        }
         getDataFromAPI();
     },[]);
 
@@ -90,17 +165,31 @@ function MTA() {
         }
     },[data]);
 
+    useEffect(()=>{
+      timerRef.current = setInterval(()=>setCount(prev => prev + 1), 1000);
+
+      /** fetch every 30 min */
+      if(count % 1800 == 0){
+        getDataFromAPI();
+        setCount(0);
+      }
+
+      if(count % 30 == 0){
+        setSingleFeed({...data[Math.floor(Math.random() * data.length)]});
+      }
+
+      return () => {clearInterval(timerRef.current);};
+    },[])
+
     return(
         <>
             {loaded ? data.map((elem, index) => (
                 <div key={elem.id} className="card" style={{width: "18rem"}}>
                     <h5 key={elem.id + "alerttype"} className="card-title">{elem.alert["transit_realtime.mercury_alert"]["alert_type"]}</h5>
-                    <div key={elem.id + "text"} dangerouslySetInnerHTML={{__html:elem.alert.header_text.translation[1].text}} className="card-text"></div>
-                    <div key={elem.id + "description"} dangerouslySetInnerHTML={{__html:elem.alert["description_text"].translation[1].text}} className="card-text"></div>
                 </div>
             )) : (<div><p>still loading</p></div>)}
         </>
     )
 }
 
-export default MTA
+export default MTAHeadline
--- src/test/fetch.js
+++ src/test/fetch.js
@@ -1,5 +1,4 @@
 export async function testAPI(){
     const header = {method:"GET",headers:{"x-api-key":"IQbkTECZFd8rfOAqS13Mj7utmZ4sf8ZC6pqG8on5"}};
-    return await fetch("https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fbus-alerts.json",header)
-        
+    return await fetch("https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fbus-alerts.json",header).catch(err => console.error(err));
 }
