4b65ae757b425725250d2ae394bff920069c88d6
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-09-17 13:42:28 -0400

    Update weather components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Newspage from "./componets/Newspage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavBar from "./componets/NavBar"
+import NavBar from "./componets/NavBar";
 import AboutMe from "./componets/AboutMe";
-import MTA from './components/MTA'
-import MTAHeadline from './components/MTAHeadline'
-
+import MTA from "./components/MTA";
+import MTAHeadline from "./components/MTAHeadline";
+import CurrentWeather from "./weather-components/current-weather/current-weather";
 
 function App() {
   const NEWS = import.meta.env.VITE_NEWS_KEY;
   // const BUS = import.meta.env.VITE_API_KEY;
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
 
   return (
     <Router>
       <NavBar />
       <div className="container">
+        <Search onSearchChange={handleOnSearchChange} />
         <Routes>
           <Route path="/" element={<Newspage NEWS={NEWS} />} />
           <Route path="/about" element={<AboutMe />} />
+          <Route path="/currentWeather" element={<CurrentWeather />} />
         </Routes>
       </div>
     </Router>
