cdd7563d30e0096203398826f831e90fc7cb1e44
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-09-18 18:49:30 -0400

    minor fix for weather alert's position

--- src/components/weather-components/current-weather/current-weather.jsx
+++ src/components/weather-components/current-weather/current-weather.jsx
@@ -5,22 +5,24 @@ import NationalWeatherAlert from "../../NationalWeatherAlert";
 
 const CurrentWeather = () => {
   return (
-    <div className="weather">
-      <div className="top">
-        <Search />
+    <>
+      <div className="weather">
+        <div className="top">
+          <Search />
+        </div>
+        <div className="bottom">
+          <h1 className="heading">Your Local Weather</h1>
+          <p className="city">Belgrade</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img
+          alt="weather"
+          className="weather-icon"
+          src="icons/weather-symbol.png"
+        />
       </div>
-      <div className="bottom">
-        <h1 className="heading">Your Local Weather</h1>
-        <p className="city">Belgrade</p>
-        <p className="weather-description">Sunny</p>
-      </div>
-      <img
-        alt="weather"
-        className="weather-icon"
-        src="icons/weather-symbol.png"
-      />
       <NationalWeatherAlert />
-    </div>
+    </>
   );
 };
 
